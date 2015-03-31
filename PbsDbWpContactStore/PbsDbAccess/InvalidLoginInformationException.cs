c792ec8fc7b0ead7be305d10c19ebf456005a411
jonasmarty@hotmail.com <2Ap3A%n3-*@!>
2015-04-01 00:41:44 +0200

    Accessing the API work now. Interpretation fails...

---
+++ PbsDbWpContactStore/PbsDbAccess/InvalidLoginInformationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PbsDbAccess
+{
+	public class InvalidLoginInformationException : Exception
+	{
+		private const string DefaultMessage = "Passwort oder email falsch.";
+
+		public InvalidLoginInformationException()
+			: this(DefaultMessage)
+		{
+		}
+
+		public InvalidLoginInformationException(string message)
+			: base(message)
+		{
+		}
+
+		public InvalidLoginInformationException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+
+	}
+}
---
+++ PbsDbWpContactStore/PbsDbAccessTestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using PbsDbAccess;
+
+namespace PbsDbAccessTestConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LoggedinUserInformation userInfo = null;
+			var task = Task.Run(async () => userInfo = await PbsDbAccess.PbsDbAccess.RecieveUserInformationAsync("wiwo@outlook.com", "J268210pfadi"));
+
+
+			try
+			{
+				task.Wait();
+				Console.WriteLine(userInfo.Token);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine(task.Exception.InnerException.Message);
+
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
